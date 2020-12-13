9ce9d05147fd7e91475cb56a64d84f9d5b099f77
Gellifree <forge.delfadortitan@gmail.com>
2020-12-13 22:15:39 +0100

    Kattintás érzékelése, segédfüggvények megírása BUGOS

--- GodotSweeper/Scenes & Scripts/PlayGroundScene.gd
+++ GodotSweeper/Scenes & Scripts/PlayGroundScene.gd
@@ -5,6 +5,9 @@ var alive = true
 
 var map = []
 var bombCount = []
+var marker = []
+#Az első kattintásnál sosem lehet akna, ezért figyeljük hogy az első kattintás-e, 
+#és ha igen, akkor a kattintás helyéről ha van, elvesszük az aknát, és körülötte is!
 var firstClick = 0
 
 func generateMap(size):
@@ -21,7 +24,7 @@ func generateMap(size):
 	for i in range(size):
 		for j in range(size):
 			var rnd = rngen.randf_range(0, 100)
-			if(rnd > 80):
+			if(rnd > 90):
 				map[i][j] = 1
 		print(map[i])
 
@@ -67,8 +70,9 @@ func sumData(list):
 		sum += list[i]
 	return sum
 
-func generateBombCount(map, bombCount):
+func generateBombCount():
 	print("Bomba megszámlásáért felelős tömb készítése\n")
+	bombCount = []
 	for i in range(map.size()):
 		bombCount.append([])
 		for j in range(map.size()):
@@ -86,30 +90,109 @@ func generateBombCount(map, bombCount):
 			bombCount[i][j] = sumData(summableList)
 		print(bombCount[i])
 
+func generateMarker():
+	for i in range(map.size()):
+		marker.append([])
+		for j in range(map.size()):
+			marker[i].append(0)
+
+
 
 func Reveal():
 	for i in range(map.size()):
 		for j in range(map.size()):
 			if(map[i][j] == 1):
-				$PlayableTiles.set_cell(j,i,11)
+				$PlayableTiles.set_cell(i,j,11)
 				pass
 			else:
-				$PlayableTiles.set_cell(j,i,bombCount[i][j])
+				$PlayableTiles.set_cell(i,j,bombCount[i][j])
 				#if(bombCount[i][j] == 0):
 				#	$PlayableTiles.set_cell(j, i, 9)
 				pass
 
+func revealSingleTile(point):
+	$PlayableTiles.set_cell(point[0], point[1], bombCount[point[0]][point[1]])
+	pass
+
+func guessTile(point):
+	if(map[point[0]][point[1]] == 1):
+		alive = false
+		print("You lose - Game over")
+		Reveal()
+	else:
+		if(bombCount[point[0]][point[1]] != 0):
+			revealSingleTile(point)
+			marker[point[0]][point[1]] == 1
+		else:
+			revealZeroes(point)
+	pass
+
+func revealList(list):
+	for i in range(len(list)):
+		revealSingleTile(list[i])
+
+
+func revealZeroes(point):
+	var validBorder = validBorders(point)
+	validBorder.append(point)
+	
+	revealList(validBorder)
+	var zeroes = []
+	
+	for i in range(len(validBorder)):
+		if(bombCount[validBorder[i][0]][validBorder[i][1]] == 0 and marker[validBorder[i][0]][validBorder[i][1]] != 1):
+			zeroes.append(validBorder[i])
+	if(bombCount[point[0]][point[1]] == 0):
+		for i in range(len(validBorder)):
+			marker[validBorder[i][0]][validBorder[i][1]] = 1
+		for i in range(len(zeroes)):
+			revealZeroes(zeroes[i])
+	
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Töltsük ki a játékteret
-	setTileMap(5)
+	setTileMap(4)
 	#Hozzuk létre a pályát, ami tárolja hogy hol vannak a bombák
 	
-	generateMap(5)
-	generateBombCount(map, bombCount)
-	Reveal()
+	generateMap(4)
+	generateBombCount()
+	generateMarker()
+	#Reveal()
+	#guessTile([1,1])
 	pass
 
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		var eventPos = event.position
+		#eventPos.x -= 30
+		#eventPos.y -= 30
+		
+		var tile_index = $PlayableTiles.world_to_map(eventPos)
+		tile_index[0] = int(tile_index[0] / 2)  #Osztani kell a skálázással
+		tile_index[1] = int(tile_index[1] / 2)
+		
+		var buttonState = event.get_button_index()
+		print("buttonstate:",buttonState)
+		
+		if(buttonState == 1 and alive == true):
+			print("Mouse click at: ", tile_index) 
+			if(tile_index[0] >= 0 and tile_index[0] < len(map) and tile_index[1] >= 0 and tile_index[1] < len(map)):
+				print("Firstclick értéke: ",firstClick)
+				if(firstClick == 0):
+					map[tile_index[0]][tile_index[1]] = 0
+					print("map:",map[tile_index[0]][tile_index[1]])
+					var validBorder = validBorders(tile_index)
+					for i in range(len(validBorder)):
+						map[validBorder[i][0]][validBorder[i][1]] == 0
+					firstClick += 1
+					generateBombCount()
+					
+				guessTile([tile_index[0], tile_index[1]])
+					
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
